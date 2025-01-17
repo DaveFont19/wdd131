f677201a089b8f528783354f4e0e5f185cdc0e72
DaveFont19 <dfontes@byupathway.edu>
2025-01-16 20:24:02 -0600

    Handlig DOM Events

--- week02/script/bom.js
+++ week02/script/bom.js
@@ -1,10 +1,24 @@
 const input = document.querySelector('#favchap'); //Esto permite que se pueda manipular la etiqueta con id favchap
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
-const li = document.createElement('li'); //Esto crea un elemento li en el HTML
-const buttonDelete = document.createElement('button') //crea un boton
 
-li.textContent = input.value;// aquí obtiene los valores de el input
-buttonDelete.textContent = 'X'; // se le pone un signo al boton
-li.append(buttonDelete);
-list.append(li); // agrega la list a li
+
+
+button.addEventListener('click', function () {
+    if (input.value.trim() !== '') {
+
+        const li = document.createElement('li'); //Esto crea un elemento li en el HTML
+        const deleteButton = document.createElement('button') //crea un boton
+
+        li.textContent = input.value;// aquí obtiene los valores de el input
+        deleteButton.textContent = '❌';
+        li.appendChild(deleteButton);
+        list.appendChild(li); // agrega la list a li
+
+        input.value = '';
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li); //Borra el elemento que tiene el deleteButton
+
+            input.focus()
+        })
+}});
